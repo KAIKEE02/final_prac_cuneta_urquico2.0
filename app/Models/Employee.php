7be43d88c9241e5d06f1f2d0921c2d495b22d88e
unknown <2023310508@pampangastateu.edu.ph>
2026-05-23 14:24:12 +0800

    sorry commited na

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Employee extends Model
 {
-    //
+      use HasFactory;
+    protected $table = 'employees';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'fname',
+        'lname',
+        'mname',
+        'age',
+        'dobirth',
+    ];
 }
--- database/migrations/2026_05_23_052054_emp_table.php
+++ database/migrations/2026_05_23_052054_emp_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('3a_tbl', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->id();
             $table->string('fname');
             $table->string('lname');
@@ -29,6 +29,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('3a_tbl');
-            
+
     }
 };
